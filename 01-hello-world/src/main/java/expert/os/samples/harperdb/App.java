1fe9770c2dfab843adf4cbcf4ea78ed75ed4d793
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-05 15:54:24 +0000

    style: alling code with documentation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 01-hello-world/src/main/java/expert/os/samples/harperdb/App.java
+++ 01-hello-world/src/main/java/expert/os/samples/harperdb/App.java
@@ -2,16 +2,23 @@
 package expert.os.samples.harperdb;
 
 
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
 
 public class App {
 
     public static void main(String[] args) {
+        //Create an instance of JCache's caching provider to get JCacheManager's instance by NCache
+        CachingProvider provider = Caching.getCachingProvider();
+        CacheManager manager = provider.getCacheManager();
+        //Get a cache from manager via its string name.
+        javax.cache.Cache jCache = manager.getCache("demoCache");
 
     }
 
     private App() {
     }
-
 
 
 }
