2bdddf1fabfb5e79526d9ebc88ff543d2dcafc06
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-05 16:31:39 +0000

    feat: create doncintion cache
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 01-hello-world/src/main/java/expert/os/samples/harperdb/App.java
+++ 01-hello-world/src/main/java/expert/os/samples/harperdb/App.java
@@ -9,11 +9,16 @@ import javax.cache.spi.CachingProvider;
 public class App {
 
     public static void main(String[] args) {
-        //Create an instance of JCache's caching provider to get JCacheManager's instance by NCache
+        // Create an instance of JCache's caching provider to get JCacheManager's instance by NCache
         CachingProvider provider = Caching.getCachingProvider();
         CacheManager manager = provider.getCacheManager();
-        //Get a cache from manager via its string name.
-        javax.cache.Cache jCache = manager.getCache("demoCache");
+        // Get a cache from manager via its string name.
+        javax.cache.Cache<Object, Object> jCache = manager.getCache("demoCache");
+        jCache.put("key", "value");
+        System.out.println("Caching with NCache: " + jCache.get("key"));
+        /*Remove data from the cache*/
+        Boolean result = jCache.remove("key");
+
 
     }
 
